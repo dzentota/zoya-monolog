270464384d325f9d1d40d22b097ddc493cdc97bc
Alex Tatulchenkov <webtota@gmail.com>
2015-01-14 11:01:27 +0300

    Removed error and exception handlers

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,15 +4,3 @@ defined('ZOYA_ROOT')
     
 error_reporting(-1);
 
-set_error_handler(
-    function ($errNo, $errStr, $errFile, $errLine) {
-        throw new ErrorException($errStr, $errNo, 0, $errFile, $errLine);
-    }
-);
-
-set_exception_handler(
-    function (Exception $e) {
-        fwrite(STDERR, $e->getMessage() . PHP_EOL);
-        fwrite(STDERR, $e->getTraceAsString() . PHP_EOL);
-    }
-);
